6596c61e38b6dd25b37343c3ea7a7d4e9d13c87f
shrikha56 <shrikha56@gmail.com>
2025-03-18 20:36:49 +1100

    Update configuration files

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'], // Add other image domains as needed
+  },
+};
+
+export default nextConfig; 
